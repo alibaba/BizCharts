6a103d4de053d34b13deaca9e8ceabf1b66d41e2
hm.hm <hm.hm@alibaba-inc.com>
2018-03-26 16:37:27 +0800

    fix slider update compare props change

--- plugin/slider/src/index.jsx
+++ plugin/slider/src/index.jsx
@@ -4,73 +4,12 @@ import React, { Component } from 'react';
 const SliderAttrs = ['width', 'height', 'padding', 'xAis', 'yAxis', 'start', 'end',
   'fillerStyle', 'backgroundStyle', 'scales', 'textStyle', 'handleStyle', 'backgroundChart'];
 
-function isArray(obj) { return Array.isArray(obj); }
-function isObject(obj) { return typeof obj === 'object' && obj !== null; }
-
-function length(obj) {
-  if (isArray(obj)) {
-    return obj.length;
-  } else if (isObject(obj)) {
-    return Object.keys(obj).length;
-  }
-
-  return 0;
-}
-
-
-function is(x, y) {
-  if (x === y) {
-    return x !== 0 || y !== 0 || 1 / x === 1 / y;
-  }
-  return x !== x && y !== y; //  NaN == NaN
-}
-
-function each(obj, fun) {
-  if (obj.forEach) {
-    obj.forEach(fun);
-  }
-  if (isObject(obj)) {
-    Object.keys(obj).forEach((key) => {
-      fun(obj[key], key);
-    });
-  }
-}
-
-function shallowEqual(objA, objB) {
-  if (is(objA, objB)) {
-    return true;
-  }
-
-  if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) {
-    return false;
-  }
-
-  if (isArray(objA) !== isArray(objB)) {
-    return false;
-  }
-
-  if (length(objA) !== length(objB)) {
-    return false;
-  }
-
-  let ret = true;
-
-  each(objA, (v, k) => {
-    if (!is(v, objB[k])) {
-      return (ret = false);
-    }
-    return true;
-  });
-
-  return ret;
-}
-
 function sliderNeedRebuild(props, nextProps) {
   if (props.onChange !== nextProps.onChange) { return true; }
 
   for (let i = 0; i < SliderAttrs.length; i += 1) {
     const attr = SliderAttrs[i];
-    if (!shallowEqual(props[attr], nextProps[attr])) {
+    if (!window.G2.Util.isEqual(props[attr], nextProps[attr])) {
       return true;
     }
   }
@@ -79,6 +18,10 @@ function sliderNeedRebuild(props, nextProps) {
 }
 
 class Slider extends Component {
+  constructor() {
+    super();
+    this.reBuild = false;
+  }
 
   componentDidMount() {
     const slider = this.createG2Instance();
