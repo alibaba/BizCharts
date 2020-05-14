2e6eec1512415dda60837d91f45df8f937569f73
leanne <811873539@qq.com>
2020-05-14 10:52:27 +0800

    fix: 去掉readonly
    如题

--- src/components/Facet/index.tsx
+++ src/components/Facet/index.tsx
@@ -22,16 +22,18 @@ registerFacet('tree', Tree);
 export interface IFacetProps extends FacetCfg<any>, React.Props<any> {
   type: "circle" | "rect" | "mirror" | "list" | "matrix" | "tree";
   /** 行标题的样式。 */
-  readonly columnTitle?: FacetTitle,
+  columnTitle?: FacetTitle,
   /** 列标题的样式。 */
-  readonly rowTitle?: FacetTitle,
+  rowTitle?: FacetTitle,
   /** 是否转置。 */
-  readonly transpose?: boolean;
+  transpose?: boolean;
   /** 标题样式。 */
-  readonly title?: FacetTitle;
+  title?: FacetTitle;
   /** 指定每行可显示分面的个数，超出时会自动换行。 */
-  readonly cols?: number;
-  readonly line?: Line;
+  cols?: number;
+  line?: Line;
+  /** 绘制每个分面 **/
+  eachView: (view, facet) => void;
 };
 
 function Facet(props: IFacetProps) {
