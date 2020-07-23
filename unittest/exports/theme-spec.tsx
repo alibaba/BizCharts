369b11e31f5a5b40c7d7fa6fc2bbd6deee12ab31
以昕 <huanian.chn@alibaba-inc.com>
2020-07-23 11:41:39 +0800

    chord: 说明修改

---
+++ unittest/exports/theme-spec.tsx
@@ -0,0 +1,19 @@
+import { registerShape, registerTheme, getTheme } from '../../src/core';
+import { registerPlotTheme } from '../../src';
+
+
+describe('export function', () => {
+  it('should export registerShape', () => {
+    expect(registerShape).toBeDefined();
+  });
+  it('should export registerTheme', () => {
+    expect(registerTheme).toBeDefined();
+  });
+  it('should export getTheme', () => {
+    expect(getTheme).toBeDefined();
+  });
+  it('should export registerPlotTheme', () => {
+    expect(registerPlotTheme).toBeDefined();
+  });
+})
+
