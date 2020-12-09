eca2cd2d2c5040d3f2e671d598137abd97ba6154
wb-tyz279201 <wb-tyz279201@alibaba-inc.com>
2020-12-09 16:20:25 +0800

    feat:groupedRoseChart对齐G2.plots最新API,并通过测试用例；PieChart加上polyfillOptions对api进行转译

--- src/plots/GroupedRoseChart.tsx
+++ src/plots/GroupedRoseChart.tsx
@@ -1,12 +1,41 @@
 import 'react';
-import GroupedRose, { RoseOptions } from '@antv/g2plot/lib/plots/rose';
-import createPlot from '../createPlot';
 import warn from 'warning';
+import { Rose, RoseOptions as options } from '@antv/g2plot/lib/plots/rose';
+import createPlot, { BasePlotOptions } from '../createPlot';
+import { polyfillOptions } from './core/polyfill';
+import { replaceApi } from './core/replaceApi';
 
-interface GroupedRoseOptions extends RoseOptions {};
+const REPLACEAPILIST = [{
+  sourceKey: 'groupField',
+  targetKey: 'seriesField',
+  notice: '请使用seriesField替代',
+}, {
+  sourceKey: 'categoryField',
+  targetKey: 'xField',
+  notice: '请使用xField替代',
+}, {
+  sourceKey: 'radiusField',
+  targetKey: 'yField',
+  notice: '请使用yFeild替代',
+}];
+
+interface GroupedRoseOptions extends options, BasePlotOptions {
+  /** 请使用seriesField替代 */
+  groupField?: string;
+  /** 请使用xField替代 */
+  categoryField?: string;
+  /** 请使用yFeild替代 */
+  radiusField?: string;
+}
+
+const polyfill = (opt: GroupedRoseOptions): GroupedRoseOptions => {
+  warn(true, '<GroupedRose /> 即将在4.2.0后废弃，请使用<Rose />。文档查看：')
+
+  const options = polyfillOptions(opt);
+  replaceApi(REPLACEAPILIST,options);
+  return { ...options, isGroup: true };
+}
 
 export { GroupedRoseOptions };
-export default createPlot<GroupedRoseOptions>(GroupedRose, 'GroupedRoseChart', props => {
-  warn(true, '<GroupedRose /> 即将在4.2.0后废弃，请使用<Rose />。文档查看：')
-  return props;
-});
+
+export default createPlot<GroupedRoseOptions>(Rose, 'GroupedRoseChart', polyfill);
--- src/plots/PieChart.tsx
+++ src/plots/PieChart.tsx
@@ -1,8 +1,12 @@
 import 'react';
 import { Pie, PieOptions as options } from '@antv/g2plot/lib/plots/pie';
 import createPlot, { BasePlotOptions } from '../createPlot';
+import { polyfillOptions } from './core/polyfill';
 
 interface PieOptions extends options, BasePlotOptions {};
 
+const polyfill = (opt: PieOptions): PieOptions =>
+    polyfillOptions(opt);
+
 export { PieOptions };
-export default createPlot<PieOptions>(Pie, 'PieChart');
+export default createPlot<PieOptions>(Pie, 'PieChart', polyfill);
