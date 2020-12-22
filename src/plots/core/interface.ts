550bd98dbd6e4409d81861063565fbdf42d1b118
wb-tyz279201 <wb-tyz279201@alibaba-inc.com>
2020-12-22 12:26:15 +0800

    feat: 饼图：legend tooltip label 这三个API重新定义interface 及重新替换api  及新增测试用例

--- src/plots/core/interface.ts
+++ src/plots/core/interface.ts
@@ -1,25 +1,27 @@
-import { GeometryLabelCfg, LegendCfg } from '@antv/g2/lib/interface';
+import { GeometryLabelCfg, LegendCfg, G2LegendTitleCfg } from '@antv/g2/lib/interface';
 import { TooltipOptions } from '@antv/g2plot/lib/types/tooltip'
 
 
-
-
-export interface LengendAPIOptions extends LegendCfg {
+interface LegendTitle extends G2LegendTitleCfg {
     visible?: boolean,
-     /**
-     * 请使用itemName.formatter替代
-     * **分类图例适用**，图例项 name 文本的配置。
-     * 属性结构如下：
-     *
-     * ```ts
-     * {
-     *   spacing?: number; // 图例项 name 同后面 value 的间距
-     *   formatter?: (text: string, item: ListItem, index: number) => any; // 格式化文本函数
-     *   style?: ShapeAttrs; // 文本配置项
-     * }
-     * ```
-     */
-    formatter?: LegendCfg['itemName']['formatter'],
+}
+
+export interface LengendOptions extends LegendCfg {
+    visible?: boolean;
+    /**
+    * 请使用itemName.formatter替代
+    * **分类图例适用**，图例项 name 文本的配置。
+    * 属性结构如下：
+    *
+    * ```ts
+    * {
+    *   spacing?: number; // 图例项 name 同后面 value 的间距
+    *   formatter?: (text: string, item: ListItem, index: number) => any; // 格式化文本函数
+    *   style?: ShapeAttrs; // 文本配置项
+    * }
+    * ```
+    */
+    formatter?: LegendCfg['itemName']['formatter'];
     /**
      * 请使用itemName替代
      * **分类图例适用**，图例项 name 文本的配置。
@@ -33,14 +35,27 @@ export interface LengendAPIOptions extends LegendCfg {
      * }
      * ```
      */
-    text?: LegendCfg['itemName'],
+    text?: LegendCfg['itemName'];
+    title?: LegendTitle;
 }
 
-export interface TooltipAPIOptions extends TooltipOptions {
-    visible?: boolean,
+export declare type LengendAPIOptions = false | LengendOptions;
+
+
+
+
+export interface TooltipCfg extends TooltipOptions {
+    visible?: boolean;
 }
 
-export interface LabelAPIOptions extends GeometryLabelCfg {
-    visible?: boolean,
+export declare type TooltipAPIOptions = false | TooltipCfg;
+
+
+
+
+export interface LabelOptions extends GeometryLabelCfg {
+    visible?: boolean;
     readonly formatter?: GeometryLabelCfg['content'];
 }
+
+export declare type LabelAPIOptions = false | LabelOptions;
