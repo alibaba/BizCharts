c1e716d0c4c13c0d7f074c93d67b0a5beaf16ad6
以昕 <huanian.chn@alibaba-inc.com>
2020-12-23 16:58:55 +0800

    fix: preData 数据缓存bug

--- demos/plot/plots.jsx
+++ demos/plot/plots.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { useState, useMemo, useEffect } from "react";
-import { LineChart, BarChart, GaugeChart, Chart, Coordinate, Tooltip, Interval, View } from '../../src';
+import React, { useState } from "react";
+import { BarChart, GaugeChart, Chart, Coordinate, Tooltip, Interval } from '../../src';
 
-import { Text } from '../../src/components/Annotation';
+// import { Text } from '../../src/components/Annotation';
 // import Tooltip from '../../src/components/Tooltip';
 
-
-function Pie ({ data }) {
+function Pie({ data }) {
   return (
-    <Chart pure height={100}  width={100} data={data} autoFit>
+    <Chart pure height={100} width={100} data={data} autoFit>
       <Coordinate type="polar" innerRadius={0.2} />
       <Interval
         position="year*population"
@@ -46,43 +45,43 @@ const data = [
   }
 ];
 
-function Basic(options) {
+function Basic() {
   const [value, setValue] = useState(1);
   const bar = React.createRef();
   const [autoFit, setAutoFit] = useState(true);
   return (
     <div>
-      
+
       <div onClick={() => {
-        setValue(value+1);
+        setValue(value + 1);
         setAutoFit(!autoFit);
       }}>click me</div>
       <GaugeChart
-				title={{
-					visible: true,
-					text: '仪表盘',
-				}}
-				width={400}
-				height={400}
-				value={64}
-				min={0}
-				max={100}
-				range={[0, 25, 50, 75, 100]}
-				color={['#39B8FF', '#52619B', '#43E089', '#C0EDF3']}
-				statistic={{
-					visible: true,
-					text: '优',
-					color: '#30bf78',
-				}}
-			/>
+        title={{
+          visible: true,
+          text: '仪表盘',
+        }}
+        width={400}
+        height={400}
+        value={Math.random() * 100}
+        min={0}
+        max={100}
+        range={[0, 25, 50, 75, 100]}
+        color={['#39B8FF', '#52619B', '#43E089', '#C0EDF3']}
+        statistic={{
+          visible: true,
+          text: '优',
+          color: '#30bf78',
+        }}
+      />
       <BarChart
         title="图表标题"
         data={data}
         yField="year"
         xField="value"
-       >
+      >
         <Tooltip>
-          {(title, items, x, y) => {
+          {() => {
             const MOCK_DATA = [
               { year: '2001', population: 41.8 },
               { year: '2002', population: 38 },
@@ -100,11 +99,15 @@ function Basic(options) {
             return <Pie data={MOCK_DATA} />
           }}
         </Tooltip>
-       </BarChart>
-       <BarChart ref={bar} onGetG2Instance={(c) => {
-         console.log('onGetG2Instance', c)
-       }} onAfterrender={() => {console.log(1)}} />
-       
+      </BarChart>
+      <BarChart
+        ref={bar}
+        onGetG2Instance={() => {
+          // console.log('onGetG2Instance', c)
+        }}
+        onAfterrender={() => { console.log('onAfterrender') }}
+      />
+
     </div>
   );
 }
