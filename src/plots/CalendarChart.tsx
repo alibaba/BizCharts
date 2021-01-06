6086e806b0add9232ccd11fb8f334347a920139e
以昕 <huanian.chn@alibaba-inc.com>
2021-01-06 20:54:13 +0800

    feat: g2Plot 升级日历图

--- src/plots/CalendarChart.tsx
+++ src/plots/CalendarChart.tsx
@@ -1,12 +1,14 @@
 import 'react';
 import { Heatmap, HeatmapOptions } from '@antv/g2plot/lib/plots/heatmap';
+import { ColorAttr } from '@antv/g2plot/lib/types/attr'
 import createPlot, { BasePlotOptions } from '../createPlot';
-import { registerShape } from '../core';
-import { polyfillOptions } from './core/polyfill';
-import { get, set } from '@antv/util';
+import { G2 } from '@antv/g2plot';
+import { polyfillOptions, replaceApi } from './core/polyfill';
+import { get, isNil, set } from '@antv/util';
+import { LengendAPIOptions, TooltipAPIOptions, LabelAPIOptions } from './core/interface';
 
-registerShape('polygon', 'boundary-polygon', {
-  draw(cfg: any, container) {
+G2.registerShape('polygon', 'boundary-polygon', {
+  draw(cfg, container) {
     const group = container.addGroup();
     const attrs = {
       stroke: '#fff',
@@ -14,7 +16,7 @@ registerShape('polygon', 'boundary-polygon', {
       fill: cfg.color,
       paht: [],
     };
-    const points = cfg.points as any;
+    const points = cfg.points;
     const path = [
       ['M', points[0].x, points[0].y],
       ['L', points[1].x, points[1].y],
@@ -58,12 +60,82 @@ registerShape('polygon', 'boundary-polygon', {
   },
 });
 
-export interface CalendarOptions extends HeatmapOptions, BasePlotOptions {}
+const REPLACEAPILIST = [{
+  sourceKey: 'colors',
+  targetKey: 'color',
+  notice: 'colors 是 g2Plot@1.0 的属性，请使用 color 属性替代',
+}, {
+  sourceKey: 'valueField',
+  targetKey: 'colorField',
+  notice: 'valueField 是 g2@1.0的属性，即将废弃，请使用colorField替代',
+}, {
+  sourceKey: 'radiusField',
+  targetKey: 'yField',
+  notice: '请使用yFeild替代',
+}];
+
+export interface CalendarOptions extends HeatmapOptions, BasePlotOptions {
+  legend?: LengendAPIOptions;
+  tooltip?: TooltipAPIOptions;
+  label?: LabelAPIOptions;
+  dateField?: string;
+  /** valueField 是 g2@1.0的属性，即将废弃，请使用colorField替代 */
+  valueField?: string;
+  dateRange?: string[];
+  /** colors 是 g2Plot@1.0 的属性，即将废弃， 请使用 color 属性替代 */
+  colors?: ColorAttr;
+}
 
 export default createPlot<CalendarOptions>(Heatmap, 'CalendarChart', (props) => {
   const options = polyfillOptions(props);
-  if (get(props, 'shape')) {
+  replaceApi(REPLACEAPILIST, options);
+  if (isNil(get(props, 'shape'))) {
     set(options, 'shape', 'boundary-polygon');
   }
+
+  // 1.0 的默认值
+  if (isNil(get(options, 'xField')) && isNil(get(options, 'yField'))) {
+    set(options, 'xField', 'week');
+    set(options, 'meta.week', {
+      type: 'cat',
+      ...get(options, 'meta.week', {})
+    });
+    set(options, 'yField', 'day');
+    set(options, 'meta.day', {
+      type: 'cat',
+      values: ['Sun.', 'Mon.', 'Tues.', 'Wed.', 'Thur.', 'Fri.', 'Sat.'],
+    });
+    set(options, 'reflect', 'y');
+    set(options, 'xAxis', {
+      tickLine: null,
+      line: null,
+      label: {
+        offset: 20,
+        style: {
+          fontSize: 12,
+          fill: '#bbb',
+          textBaseline: 'top',
+        },
+        formatter: (val) => {
+          if (val == '2') {
+            return 'MAY';
+          } else if (val === '6') {
+            return 'JUN';
+          } else if (val == '10') {
+            return 'JUL';
+          } else if (val === '14') {
+            return 'AUG';
+          } else if (val == '18') {
+            return 'SEP';
+          } else if (val === '24') {
+            return 'OCT';
+          }
+          return '';
+        },
+      },
+      ...get(options, 'xAxis', {})
+    })
+  }
+
   return options;
 });
