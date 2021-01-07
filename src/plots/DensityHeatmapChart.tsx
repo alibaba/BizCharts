64e5cf5322612107bf5c01bebaee004602f509f8
以昕 <huanian.chn@alibaba-inc.com>
2021-01-07 11:32:59 +0800

    feat: g2Plot 升级密度热力图

--- src/plots/DensityHeatmapChart.tsx
+++ src/plots/DensityHeatmapChart.tsx
@@ -1,9 +1,23 @@
 import 'react';
-import DensityHeatmap, { HeatmapOptions } from '@antv/g2plot/lib/plots/heatmap';
-import createPlot from '../createPlot';
+import { Heatmap, HeatmapOptions } from '@antv/g2plot/lib/plots/heatmap';
+import { polyfillOptions, replaceApi } from './core/polyfill';
+import createPlot, { BasePlotOptions } from '../createPlot';
+import { set } from '@antv/util';
 
-interface DensityHeatmapOptions extends HeatmapOptions {}
+interface DensityHeatmapOptions extends HeatmapOptions, BasePlotOptions {
+  /** g2Plot@1.0的api，即将废弃，请使用 `sizeRatio` 替代 */
+  radius?: Record<string,any> | number;
+}
 
 export { DensityHeatmapOptions };
 
-export default createPlot<DensityHeatmapOptions>(DensityHeatmap, 'DensityHeatmapChartChart');
+export default createPlot<DensityHeatmapOptions>(Heatmap, 'DensityHeatmapChartChart', (props) => {
+  const options = polyfillOptions(props);
+  replaceApi([{
+    sourceKey: 'radius',
+    targetKey: 'sizeRatio',
+    notice: '请使用sizeRatio替代',
+  }], options);
+  set(options, 'type', 'density');
+  return options;
+});
---
+++ unittest/plots/DensityHeatmapChart-spec.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import DensityHeatmapChart from '../../src/plots/DensityHeatmapChart';
+import { render, cleanup } from '@testing-library/react';
+
+function Demo(props) {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://alifd.alibabausercontent.com/materials/@bizcharts/heatmap-image/0.3.0/mock.json')
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+      })
+  }, [])
+  
+  return (
+    <DensityHeatmapChart
+      data={data}
+      title={{
+        visible: true,
+        text: '密度热力图',
+      }}
+      autoFit
+      {...props}
+    />
+  );
+}
+
+describe('Plots-DensityHeatmapChart', () => {
+  test('旧版密度热力图', () => {
+    let chart = null;
+    render(<Demo
+      padding="auto"
+      xField='g'
+      yField='l'
+      colorField='tmp'
+      color={['#295599', '#3e94c0', '#78c6d0', '#b4d9e4', '#fffef0', '#f9cdac', '#ec7d92', '#bc448c']}
+      radius={15}
+      onGetG2Instance={
+        (c) => chart = c
+      }
+    />)
+    expect(chart.options).toMatchSnapshot();
+    cleanup();
+  })
+  test('基础密度热力图', () => {
+    let chart = null;
+    render(<Demo
+      padding="auto"
+      xField='g'
+      yField='l'
+      colorField='tmp'
+      color="#F51D27-#FA541C-#FF8C12-#FFC838-#FAFFA8-#80FF73-#12CCCC-#1890FF-#6E32C2"
+      radius={15}
+      onGetG2Instance={
+        (c) => chart = c
+      }
+    />)
+    expect(chart.options).toMatchSnapshot();
+    cleanup();
+  });
+})
